6f02d98c23b3af7d7a7408bac87e8f38f23cee72
junghong <junghong@ninewatt.com>
2021-01-27 16:43:10 +0900

    port number is added

--- app.py
+++ app.py
@@ -26,4 +26,5 @@ def test_get_json():
 
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+    # app.run(debug=False, host='0.0.0.0'
+    app.run(host="0.0.0.0", port="5000", debug=True))
